9ed908de7f84ebb66c74f5935690b4376c810a28
nitesh-dev <niteshdev547@gmail.com>
2023-07-09 13:42:13 +0530

    playlist fetch added

--- YoutubeManager.py
+++ YoutubeManager.py
@@ -7,6 +7,11 @@ import json
 ydl_opts = {'quiet': True}
 ydl = youtube.YoutubeDL(ydl_opts)
 
+
+qualities = ['144', '240', '360','480','720','1080','2160']
+thumbResolution = ['640x480', '1280x720', '480x360', '1920x1080', '336x188']
+
+
 def getVideoDetails(url = ''):
 
     with ydl:
@@ -23,7 +28,7 @@ def getVideoDetails(url = ''):
 
         # getting video download urls
         formats = info_dict.get('formats', [])
-        qualities = ['144', '240', '360','480','720','1080','2160']
+        
         for format in formats:
             videoFormat = VideoFormat()
 
@@ -42,6 +47,44 @@ def getVideoDetails(url = ''):
 
         print(json.dumps(videoDetail.toDict()))
 
+
+def getPlaylist(url = ''):
+    with ydl:
+        info_dict = ydl.extract_info(url, download=False)
+
+        playlist = Playlist()
+        playlist.title = info_dict.get('title', '')
+
+        for item in info_dict.get('entries', []):
+            playlistVideo = PlaylistVideo()
+
+            playlistVideo.title = item.get('title', '')
+            playlistVideo.views = item.get('view_count', 0)
+            playlistVideo.likes = item.get('like_count', 0)
+            playlistVideo.dislikes = item.get('dislike_count', 0)
+            playlistVideo.url = item.get('webpage_url', '')
+
+            
+            # selecting thumbnail
+            thumbnails = item.get('thumbnails', [])
+            isThumbnailFound = False
+
+            for index in range(len(thumbnails), 0, -1):
+                thumbnail = thumbnails[index - 1]
+                resolution = thumbnail.get('resolution', '')
+
+                for res in thumbResolution:
+                    if(res == resolution):
+                        playlistVideo.thumbnail = thumbnail.get('url', '')
+                        isThumbnailFound = True
+                        break
+                
+                if isThumbnailFound == True:
+                    break
+
+            playlist.videos.append(playlistVideo)
+        
+        print(json.dumps(playlist.toDict())) 
 
 
 
@@ -98,8 +141,48 @@ class VideoFormat:
 
         return dic
 
+class Playlist:
+    title = ''
+    videos = []
 
+    def toDict(self):
+        dic = {
+            "title" : self.title,
+            "videos" : []
+        }
+
+        videoDic = []
+
+        # loop through all videos
+        for item in self.videos:
+            videoDic.append(item.toDict())
+
+        dic['videos'] = videoDic
+        return dic
+
+
+
+class PlaylistVideo:
+    title = ''
+    likes = 0
+    dislikes = 0
+    views = 0
+    thumbnail = ''
+    url = ''
+
+    def toDict(self):
+        dic = {
+            "title" : self.title,
+            "likes" : self.likes,
+            "dislikes" : self.dislikes,
+            "views" : self.views,
+            "thumbnail" : self.thumbnail,
+            "url": self.url
+        }
+
+        return dic
 
 # getVideoDetails('https://www.youtube.com/watch?v=d-GQq792pWI')
-getVideoDetails('https://www.youtube.com/watch?v=7PIji8OubXU')
+# getVideoDetails('https://www.youtube.com/watch?v=7PIji8OubXU')
+getPlaylist('https://www.youtube.com/playlist?list=PLPwpWyfm6JADGP7TZF-FzCtP9Nm-g5B8v')
 
